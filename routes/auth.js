2f426a11e9c683ec2d91215de4580f1ba4fd03da
Kelvin Brito <britok30@gmail.com>
2019-10-26 13:09:48 -0400

    client start

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,23 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const config = require("config");
+
 const { check, validationResult } = require("express-validator");
 
 //@route       GET  api/auth
 //@desc        Get logged in user
 //@access      Private
 
-router.get("/", (req, res) => {
-  res.send("Get logged in user ");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await user.findbById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 
 //@route       POST  api/users
